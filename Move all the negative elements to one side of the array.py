2187a81d1925444f9e213c1f6d81a2b6ee5ea6cc
Yash Chaturvedi <71589899+yash12-cha@users.noreply.github.com>
2021-08-13 13:34:19 +0530

    Create Move all the negative elements to one side of the array.py

---
+++ Move all the negative elements to one side of the array.py
@@ -0,0 +1,12 @@
+N = int(input("Enter number of elements: "))
+Arr = list(map(int,input("Enter the elements: ").strip().split()))[:N]
+i = -1
+pivot = 0 # Pivot element
+for j in range(0, N) :
+        if (Arr[j] < 0) :
+            i = i + 1
+            temp = Arr[i]
+            Arr[i] = Arr[j]
+            Arr[j]= temp
+print(*Arr)   
+    
