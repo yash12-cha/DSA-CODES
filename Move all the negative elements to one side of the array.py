f0c705071a6ebefbac0250e30fc5a97b7fb40282
Yash Chaturvedi <71589899+yash12-cha@users.noreply.github.com>
2021-09-08 21:51:36 +0530

    Update Move all the negative elements to one side of the array.py

--- Move all the negative elements to one side of the array.py
+++ Move all the negative elements to one side of the array.py
@@ -3,7 +3,7 @@ Arr = list(map(int,input("Enter the elements: ").strip().split()))[:N]
 i = -1
 pivot = 0 # Pivot element
 for j in range(0, N) :
-        if (Arr[j] < 0) :
+        if (Arr[j] < pivot) :
             i = i + 1
             temp = Arr[i]
             Arr[i] = Arr[j]
