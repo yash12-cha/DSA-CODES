401bb7310fc6c7cecbe835cee954ac166ea99fbb
Yash Chaturvedi <71589899+yash12-cha@users.noreply.github.com>
2022-02-04 14:08:57 +0530

    Delete Cyclically rotate an array by one.py

--- Cyclically rotate an array by one.py
+++
@@ -1,8 +0,0 @@
-N = int(input("Enter number of elements: "))
-Arr = list(map(int,input("Enter the elements: ").strip().split()))[:N]
-last = Arr[N-1]
-for i in range(N-1,0,-1):
-    Arr[i] = Arr[i-1]
-Arr[0] = last
-print(*Arr)
-    
