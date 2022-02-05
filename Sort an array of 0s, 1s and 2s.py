0c6bc4892965a11708769b22136c30b9ecc90c55
Yash Chaturvedi <71589899+yash12-cha@users.noreply.github.com>
2022-02-05 11:24:32 +0530

    Create Sort an array of 0s, 1s and 2s.py

---
+++ Sort an array of 0s, 1s and 2s.py
@@ -0,0 +1,25 @@
+def sort012(self, arr, n):
+    c0 = 0
+    c1 = 0
+    c2 = 0
+    for i in range(n):
+        if arr[i] == 0:
+            c0 += 1
+        if arr[i] == 1:
+            c1 += 1
+        if arr[i] == 2:
+            c2 += 1
+    k = 0
+    for i in range(c0):
+        arr[k] = 0
+        k += 1
+    for i in range(c1):
+        arr[k] = 1
+        k += 1
+    for i in range(c2):
+        arr[k] = 2
+        k += 1
+
+    return arr
+
+# Time Complexity: O(n)
