e4e078c7ee44f08b60df814cbc4d9b6ad3bfafa9
Yash Chaturvedi <71589899+yash12-cha@users.noreply.github.com>
2022-02-16 22:08:46 +0530

    Create BFS of graph.py

---
+++ BFS of graph.py
@@ -0,0 +1,22 @@
+from collections import deque
+class Solution:
+    def bfsOfGraph(self, V, adj):
+        visited = []    # List to keep track of visited nodes
+        queue = deque() # Initialize a Queue
+        # Select vertex 0 as the root node
+        # Add vertex 0 to queue
+        queue.append(0)
+        # Mark vertex 0 as visited
+        visited.append(0)
+        bfs = []
+        # If the queue is not empty
+        while queue:
+            # Remove the first vertex from the queue
+            node = queue.popleft()
+            bfs.append(node)
+            # Insert child nodes of the first vertex into the queue
+            for neighbour in adj[node]:
+                if neighbour not in visited:
+                    visited.append(neighbour)
+                    queue.append(neighbour)
+        return bfs
