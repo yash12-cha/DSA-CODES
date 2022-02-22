3f76d2059235bb6ea10170ee730a3fd88282ad9f
Yash Chaturvedi <71589899+yash12-cha@users.noreply.github.com>
2022-02-22 23:28:17 +0530

    Create Kth Smallest Element.py

---
+++ Kth Smallest Element.py
@@ -0,0 +1,18 @@
+import heapq
+class Solution:
+    def kthSmallest(self,nums, l, r, k):
+        # Declare empty list
+        maxHeap = []
+        # using heapify to convert list into heap
+        heapq.heapify(maxHeap)
+        for r in range(len(nums)):
+            # Adding items to the heap using heappush
+            # function by multiplying them with -1
+            heapq.heappush(maxHeap, -1 * nums[r])
+            if len(maxHeap) > k:
+                # removes the smallest element from the max heap
+                heapq.heappop(maxHeap)
+        # removes and returns the smallest element from the max heap
+        c = heapq.heappop(maxHeap)
+        # multiplying with -1
+        return -1 * c
