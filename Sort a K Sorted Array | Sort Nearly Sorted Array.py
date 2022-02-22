cd9bdfe6933f1a0e0a04d207294b0bcd609ee2d6
Yash Chaturvedi <71589899+yash12-cha@users.noreply.github.com>
2022-02-23 00:07:41 +0530

    Update Sort a K Sorted Array | Sort Nearly Sorted Array.py

--- Sort a K Sorted Array | Sort Nearly Sorted Array.py
+++ Sort a K Sorted Array | Sort Nearly Sorted Array.py
@@ -9,8 +9,11 @@ def nearlySorted(nums, k):
         # adds an element into an existing heap
         heapq.heappush(minHeap, nums[r])
         if len(minHeap) > k:
+            # removes the smallest element from the min heap
             a = heapq.heappop(minHeap)
+            # add the removed element into the answer list
             ans.append(a)
+    # pop all remaining elements from the min-heap and assign them to the answer list
     while minHeap:
         a = heapq.heappop(minHeap)
         ans.append(a)
