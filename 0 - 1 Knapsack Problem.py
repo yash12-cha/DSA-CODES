1e0cb9a5d01fc75535c4ca6682673cda7ed7dcfb
Yash Chaturvedi <71589899+yash12-cha@users.noreply.github.com>
2022-03-01 22:10:21 +0530

    Update 0 - 1 Knapsack Problem.py

--- 0 - 1 Knapsack Problem.py
+++ 0 - 1 Knapsack Problem.py
@@ -1,8 +1,8 @@
 class Solution:
     def knapSack(self,W, wt, val, n):
         dp = [[0 for x in range(W + 1)] for x in range(n + 1)]
-        for i in range(n + 1):
-            for w in range(W + 1):
+        for i in range(1,n + 1):
+            for w in range(1,W + 1):
                 if i == 0 or w == 0:
                     dp[i][w] = 0
                 elif wt[i-1] <= w:
