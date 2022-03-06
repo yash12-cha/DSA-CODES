f765f9b0094f0bc00a2d3d8305c3e174e5af868f
Yash Chaturvedi <71589899+yash12-cha@users.noreply.github.com>
2022-03-06 14:53:18 +0530

    Create Nearest Greater to left.py

---
+++ Nearest Greater to left.py
@@ -0,0 +1,17 @@
+from collections import deque
+def nextLargerElement(arr, n):
+    stack = deque()
+    ans = []
+    for i in range(0,n,1):
+        while (len(stack) != 0 and stack[-1] <= arr[i]):
+            stack.pop()
+        if len(stack) == 0:
+            ans.append(-1)
+        else:
+            ans.append(stack[-1])
+        stack.append(arr[i])
+    return ans
+n = int(input())
+arr = list(map(int,input().split()))
+ans = nextLargerElement(arr,n)
+print(*ans)
