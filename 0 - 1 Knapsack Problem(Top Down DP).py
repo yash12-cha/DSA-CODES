a73e8512c9031878d797834137c82d55cbe35181
Yash Chaturvedi <71589899+yash12-cha@users.noreply.github.com>
2022-03-06 12:59:24 +0530

    Update 0 - 1 Knapsack Problem(Top Down DP).py

--- 0 - 1 Knapsack Problem(Top Down DP).py
+++ 0 - 1 Knapsack Problem(Top Down DP).py
@@ -3,9 +3,7 @@ class Solution:
         dp = [[0 for x in range(W + 1)] for x in range(n + 1)]
         for i in range(1,n + 1):
             for w in range(1,W + 1):
-                if i == 0 or w == 0:
-                    dp[i][w] = 0
-                elif wt[i-1] <= w:
+                if wt[i-1] <= w:
                     dp[i][w] = max(val[i-1] + dp[i-1][w-wt[i-1]], dp[i-1][w])
                 else:
                     dp[i][w] = dp[i-1][w]
