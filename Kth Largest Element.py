364914c91ce3c336614d3a622054473107699627
Yash Chaturvedi <71589899+yash12-cha@users.noreply.github.com>
2022-04-11 21:56:44 +0530

    Update Kth Largest Element.py

--- Kth Largest Element.py
+++ Kth Largest Element.py
@@ -1,19 +1,30 @@
 import heapq
-class Solution(object):
-    def findKthLargest(self, nums, k):
-        # Declare empty list
-        minHeap = []
-        # using heapify to convert list into heap
-        heapq.heapify(minHeap)
-        for r in range(len(nums)):
-            # adds an element into an existing heap
-            heappush(minHeap, nums[r])
-            if len(minHeap) > k:
-                # removes the smallest element from the min heap
-                heappop(minHeap)
-        # removes and returns the smallest element from the min heap
-        return heappop(minHeap)
+def kthLargest(nums,k):
+    n = len(nums)
+    # Declare empty list
+    minHeap = []
+    # using heapify to convert list into heap
+    heapq.heapify(minHeap)
+    for r in range(len(nums)):
+        # adds an element into an existing heap
+        heapq.heappush(minHeap, nums[r])
+        if len(minHeap) > k:
+            # removes the smallest element from the min heap
+            heapq.heappop(minHeap)
+    # removes and returns the smallest element from the min heap
+    return heapq.heappop(minHeap)
+arr = list(map(int,input().split()))
+k = int(input())
+s = kthLargest(arr,k)
+print(s)
         
 '''
+Input:-
+3 2 3 1 2 4 5 5 6
+4
+
+Output:-
+4
+
 Time Complexity: - O(nlogk)
 '''
