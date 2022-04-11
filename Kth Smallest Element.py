c215bfbd87e0ce938aac755fc2f5fe86a90f70d2
Yash Chaturvedi <71589899+yash12-cha@users.noreply.github.com>
2022-04-11 21:53:02 +0530

    Update Kth Smallest Element.py

--- Kth Smallest Element.py
+++ Kth Smallest Element.py
@@ -1,21 +1,34 @@
 import heapq
-class Solution:
-    def kthSmallest(self,nums, l, r, k):
-        # Declare empty list
-        maxHeap = []
-        # using heapify to convert list into heap
-        heapq.heapify(maxHeap)
-        for r in range(len(nums)):
-            # Adding items to the heap using heappush
-            # function by multiplying them with -1
-            heapq.heappush(maxHeap, -1 * nums[r])
-            if len(maxHeap) > k:
-                # removes the smallest element from the max heap
-                heapq.heappop(maxHeap)
-        # removes and returns the smallest element from the max heap
-        c = heapq.heappop(maxHeap)
-        # multiplying with -1
-        return -1 * c
+def kthSmallest(nums,k):
+    n = len(nums)
+    # Declare empty list
+    maxHeap = []
+    # using heapify to convert list into heap
+    heapq.heapify(maxHeap)
+    for r in range(n):
+        # Adding items to the heap using heappush
+        # function by multiplying them with -1
+        heapq.heappush(maxHeap, -1 * nums[r])
+        if len(maxHeap) > k:
+            # removes the smallest element from the max heap
+            heapq.heappop(maxHeap)
+    # removes and returns the smallest element from the max heap
+    c = heapq.heappop(maxHeap)
+    # multiplying with -1
+    return -1 * c
+arr = list(map(int,input().split()))
+k = int(input())
+s = kthSmallest(arr,k)
+print(s)
+
+
 '''
+Input: -
+2 1 4 3 2
+3
+
+Output:-
+2
+
 Time Complexity: - O(nlogk)
 '''
