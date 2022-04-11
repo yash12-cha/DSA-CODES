f7084b104836362e7f3eaac7780cd20da2b72a3e
Yash Chaturvedi <71589899+yash12-cha@users.noreply.github.com>
2022-04-11 22:23:45 +0530

    Update k largest elements .py

--- k largest elements .py
+++ k largest elements .py
@@ -1,18 +1,32 @@
 import heapq
-class Solution:
+def kLargest(nums,k):
+    n = len(nums)
+    # Declare empty list
+    minHeap = []
+    # using heapify to convert list into heap
+    heapq.heapify(minHeap)
+    for r in range(len(nums)):
+        # adds an element into an existing heap
+        heapq.heappush(minHeap, nums[r])
+        if len(minHeap) > k:
+            # removes the smallest element from the min heap
+            heapq.heappop(minHeap)
+    # sort the remaining elements in the minHeap in decreasing order
+    minHeap.sort(reverse=True)
+    # return minHeap
+    return minHeap
+arr = list(map(int,input().split()))
+k = int(input())
+s = kLargest(arr,k)
+print(*s)
 
-	def kLargest(self,nums, n, k):
-	    # Declare empty list
-        minHeap = []
-        # using heapify to convert list into heap
-        heapq.heapify(minHeap)
-        for r in range(len(nums)):
-            # adds an element into an existing heap
-            heapq.heappush(minHeap, nums[r])
-            if len(minHeap) > k:
-                # removes the smallest element from the min heap
-                heapq.heappop(minHeap)
-        # sort the remaining elements in the minHeap in decreasing order
-        minHeap.sort(reverse = True)
-        # return minHeap
-        return minHeap
+'''
+Input:-
+7 10 4 3 20 15
+3
+
+Output:-
+20 15 10
+
+Time Complexity:- O(nlogk)
+'''
