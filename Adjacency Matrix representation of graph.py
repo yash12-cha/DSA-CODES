12000caf0628bb6bc869abe6bd049ef81d91376b
Yash Chaturvedi <71589899+yash12-cha@users.noreply.github.com>
2022-07-06 19:53:09 +0530

    Update Adjacency Matrix representation of graph.py

--- Adjacency Matrix representation of graph.py
+++ Adjacency Matrix representation of graph.py
@@ -1,40 +1,17 @@
 # n ----> number of nodes
 # m ----> number of edges
-n, m = tuple(map(int, input().split()))
+n,m = tuple(map(int,input().split()))
 
 # using list comprehension
-# to initialize matrix with 0
-adjm = [[0 for i in range(n + 1)] for j in range(n + 1)]
+# to initialize Adjacency Matrix with 0
+adj = [[0 for i in range(n+1)]for j in range(n+1)]
 
-for i in range(0, m):
-    # Take input of all the edges
+for i in range(0,m):
     x, y = tuple(map(int, input().split()))
-    adjm[x][y] = 1
-    adjm[y][x] = 1
-print("Adjacency matrix of above graph is given by: ")
-for i in range(1, n + 1):
-    for j in range(1, n + 1):
-        print(adjm[i][j], end=" ")
-    print()
-    
-'''
-Input: -
-7 7
-1 2
-1 3
-2 4
-2 5
-2 6
-2 7
-7 3
-Output: -
-Adjacency matrix of above graph is given by: 
-0 1 1 0 0 0 0 
-1 0 0 1 1 1 1 
-1 0 0 0 0 0 1 
-0 1 0 0 0 0 0 
-0 1 0 0 0 0 0 
-0 1 0 0 0 0 0 
-0 1 1 0 0 0 0
-'''
+    adj[u][v] =1
+    adj[v][u] = 1
 
+    
+ '''
+ Space Complexity: O(n^2), where n is number of nodes
+ '''
