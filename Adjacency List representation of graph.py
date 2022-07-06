9964fe7b410de56badfd9676d86307e88274b41c
Yash Chaturvedi <71589899+yash12-cha@users.noreply.github.com>
2022-07-06 20:00:24 +0530

    Update Adjacency List representation of graph.py

--- Adjacency List representation of graph.py
+++ Adjacency List representation of graph.py
@@ -9,12 +9,6 @@ for i in range(0, m):
     x, y = tuple(map(int, input().split()))
     adjlist[x].append(y)
     adjlist[y].append(x)
-print("Adjacency list of above graph is given by: ")
-for i in range(1, n + 1):
-    print(i,end="->")
-    for j in adjlist[i]:
-        print(j,end=" ")
-    print()
 
 '''
 Space Complexity: O(n+2m), where n is number of nodes and m is number of edges.
