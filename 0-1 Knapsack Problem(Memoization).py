3b764718084e9728aaaa7d52bb87a07998e5a6d6
Yash Chaturvedi <71589899+yash12-cha@users.noreply.github.com>
2022-07-19 20:06:42 +0530

    Update 0-1 Knapsack Problem(Memoization).py

--- 0-1 Knapsack Problem(Memoization).py
+++ 0-1 Knapsack Problem(Memoization).py
@@ -5,6 +5,8 @@ def knapsack(wt,val,C,n):
     dp = [[-1 for x in range(C + 1)] for x in range(n + 1)]
 
     # Base Case
+    # If number of items is zero or
+    # Capacity of knapsack is zero
     if n == 0 or C == 0:
         return 0
 
@@ -19,22 +21,23 @@ def knapsack(wt,val,C,n):
     else:
         dp[n][C] = knapsack(wt,val,C,n-1)
         return dp[n][C]
-n = int(input())
-C = int(input())
-val = list(map(int,input().split()))
-wt = list(map(int,input().split()))
-ans = knapsack(wt,val,C,n)
-print(ans)
+N = int(input("Enter the number of items: "))
+C = int(input("Enter the capacity of knapsack: "))
+wt = list(map(int,input("Enter weight of items: ").split()))
+val = list(map(int,input("Enter value of items: ").split()))
+res = knapsack(wt,val,C,N)
+print("Maximum total value in the knapsack: ",res)
+
 
 '''
 Input: -
-4
-40
-10 20 30 40
-30 10 40 20
+Enter the number of items: 4
+Enter the capacity of knapsack: 40
+Enter weight of items: 30 10 40 20
+Enter value of items: 10 20 30 40
 
 Output:-
-60
+Maximum total value in the knapsack:  60
 
 Time Complexity of the above approach is O(n*C).
 '''
