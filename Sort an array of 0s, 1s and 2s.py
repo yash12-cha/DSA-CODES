9a212309f68ea0cec9279a386d42ef1ccb591ca8
Yash Chaturvedi <71589899+yash12-cha@users.noreply.github.com>
2022-07-25 21:19:37 +0530

    Update Sort an array of 0s, 1s and 2s.py

--- Sort an array of 0s, 1s and 2s.py
+++ Sort an array of 0s, 1s and 2s.py
@@ -1,43 +1,36 @@
- # Method 1: Keeping count of values
-    
-def sortColors(self, arr):
-        n = len(arr)
-        c0 = 0
-        c1 = 0
-        c2 = 0
-        for i in range(n):
-            if arr[i] == 0:
-                c0 += 1
-            if arr[i] == 1:
-                c1 += 1
-            if arr[i] == 2:
-                c2 += 1
-        k = 0
-        for i in range(c0):
-            arr[k] = 0
-            k += 1
-        for i in range(c1):
-            arr[k] = 1
-            k += 1
-        for i in range(c2):
-            arr[k] = 2
-            k += 1
+def sort012(arr, n):
+    c0, c1, c2 = 0, 0, 0
+    for i in range(n):
+        if arr[i] == 0:
+            c0 += 1
+        elif arr[i] == 1:
+            c1 += 1
+        else:
+            c2 += 1
+    k = 0
+    for i in range(c0):
+        arr[k] = 0
+        k += 1
+    for i in range(c1):
+        arr[k] = 1
+        k += 1
+    for i in range(c2):
+        arr[k] = 2
+        k += 1
 
+    return arr
+n = int(input("Input number of elements: "))
+arr = list(map(int,input("Enter elements: ").split()))
+res = sort012(arr,n)
+print("Sorted array of 0s, 1s and 2s: ",*res)
 
-# Method 2: 3-Pointer approach
+'''
+Input: -
+Input number of elements: 5
+Enter elements: 0 2 1 2 0
 
-def sortColors(self, arr):
-        n = len(arr)
-        low = 0
-        mid = 0
-        high = n-1
-        while mid <= high:
-            if arr[mid] == 0:
-                arr[low],arr[mid] = arr[mid],arr[low]
-                low += 1
-                mid += 1
-            elif arr[mid] == 1:
-                mid += 1
-            elif arr[mid] == 2:
-                arr[mid],arr[high] = arr[high],arr[mid]
-                high -= 1
+Output: -
+Sorted array of 0s, 1s and 2s:  0 0 1 2 2
+
+Time Complexity: O(n). 
+'''
