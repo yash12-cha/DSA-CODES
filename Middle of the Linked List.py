6cc5e1a184e64f413c2a5291aac610b909c2915d
Yash Chaturvedi <71589899+yash12-cha@users.noreply.github.com>
2022-08-13 13:28:26 +0530

    Update Middle of the Linked List.py

--- Middle of the Linked List.py
+++ Middle of the Linked List.py
@@ -1,12 +1,49 @@
-def middleNode(self, head):
-        slow = head
-        fast = head
-        while(fast != None and fast.next != None):
-            slow = slow.next
-            fast = fast.next.next
-        return slow
+# Creation of node of Linked list
+class Node:
+    def __init__(self,data,next = None):
+        self.data = data
+        self.next = next
+# Join nodes to get a linked list
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    # Insertion of new node at the end
+    def add(self, data):
+        new_node = Node(data)
+        if self.head == None:
+            self.head = new_node
+        else:
+            current = self.head
+            while (current.next):
+                current = current.next
+            current.next = new_node
+
+    # Function to get the middle of the linked list
+    def middleNode(self):
+        # Initialize two pointers
+        slow = self.head
+        fast = self.head
+        # Iterate till fast is null and fast's next is null (fast reaches end)
+        while (fast!= None and fast.next != None):
+            slow = slow.next        # Move the slow pointer by one position
+            fast = fast.next.next   # Move the fast pointer by two positions
+        # return the slow's data, which would be the middle element.
+        print("Middle node: ",slow.data)
         
+LL = LinkedList()
+LL.add(1)
+LL.add(2)
+LL.add(3)
+LL.add(4)
+LL.add(5)
+LL.middleNode()
+
 '''
-Time Complexity: O(n), where n is the size of the linked list.
+Output:
+Middle node:  3
 '''
- 
+
+'''
+Time Complexity: O(N/2).
+'''
