a899321d43fbf863c11c6f46a10a27d5d7fc0a55
Yash Chaturvedi <71589899+yash12-cha@users.noreply.github.com>
2023-10-20 11:41:32 +0530

    Right View of a Binary Tree.py

--- Right View of a Binary Tree.py
+++ Right View of a Binary Tree.py
@@ -1,26 +1,33 @@
+# Define a Node class for the binary tree
 class Node:
-    def __init__(self,data):
+    def __init__(self, data):
         self.data = data
         self.left = None
         self.right = None
 
-def left_view_main(root,result,level,max_level):
-    if root == None:
+# Function to find the left view of the binary tree
+def left_view_main(root, result, level, max_level):
+    if root is None:
         return
     if level > max_level[0]:
+        # If this is the leftmost node at the current level, add it to the result
         result.append(root.data)
         max_level[0] = level
+    
+    # Traverse the right subtree before the left subtree to find the left view
     left_view_main(root.right, result, level + 1, max_level)
-    left_view_main(root.left, result,level + 1, max_level)
-
+    left_view_main(root.left, result, level + 1, max_level)
 
 def left_view(root):
-    result = []
-    max_level = [-1]
-    left_view_main(root,result,0,max_level)
+    result = []       # Initialize an empty list to store the left view nodes
+    max_level = [-1]  # Use a list to keep track of the maximum level seen (initialized to -1)
+    
+    # Call the left_view_main function to find the left view
+    left_view_main(root, result, 0, max_level)
+    
     return result
 
-# Driver Code
+# Driver Code to create a binary tree
 root = Node(4)
 root.left = Node(7)
 root.right = Node(6)
@@ -29,9 +36,11 @@ root.right.left.left = Node(5)
 root.right.left.right = Node(1)
 root.right.left.right.left = Node(3)
 
-print("Right View of Tree is:", end=" ")
+# Print the left view of the binary tree
+print("Left View of Tree is:", end=" ")
 left_view_result = left_view(root)
 print(left_view_result)
+
 
 '''
 Time Complexity: The time complexity for above code is O(N), where N is the number of nodes in the binary tree. 
