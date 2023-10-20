0035eb40e69526cd7ef6c15a96c6b9e07b136d6a
Yash Chaturvedi <71589899+yash12-cha@users.noreply.github.com>
2023-10-20 11:58:11 +0530

    Search in Rotated Sorted Array.py

--- Search in Rotated Sorted Array.py
+++ Search in Rotated Sorted Array.py
@@ -1,26 +1,37 @@
+# Function to search for a target element in a rotated sorted array
 def search(nums, target):
     low = 0
     high = len(nums) - 1
     while low <= high:
         mid = (low + high) // 2
+        
         if nums[mid] == target:
-            return mid
-        # Left Part is sorted
+            return mid  # Target found at index mid
+        
+        # Check if the left part is sorted
         if nums[low] <= nums[mid]:
-            if (target >= nums[low]) and target < nums[mid]:
-                high = mid - 1
+            if (target >= nums[low]) and (target < nums[mid]):
+                high = mid - 1  # Adjust the search range to the left part
             else:
-                low = mid + 1
-        # Right Part is sorted
+                low = mid + 1  # Adjust the search range to the right part
+        
+        # Check if the right part is sorted
         else:
-            if (target > nums[mid]) and target <= nums[high]:
-                low = mid + 1
+            if (target > nums[mid]) and (target <= nums[high]):
+                low = mid + 1  # Adjust the search range to the right part
             else:
-                high = mid - 1
-    return -1
-nums = list(map(int,input("Input Array: ").split()))
+                high = mid - 1  # Adjust the search range to the left part
+    
+    return -1  # Target element not found
+
+# Input: Read the rotated sorted array and the target element
+nums = list(map(int, input("Input Array: ").split()))
 target = int(input("Input target element: "))
-print(search(nums,target))
+
+# Call the search function and print the result
+result = search(nums, target)
+print(result)
+
 
 '''
 Time Complexity: O(log N) Binary Search requires log N comparisons to find the element.
