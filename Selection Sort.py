1355c20c010919ae36ec7caebb3f67dcc4b59d8e
Yash Chaturvedi <71589899+yash12-cha@users.noreply.github.com>
2023-10-20 10:57:45 +0530

    Selection Sort.py

--- Selection Sort.py
+++ Selection Sort.py
@@ -1,19 +1,31 @@
+# Define a function for Selection Sort
 def selectionSort(Arr, N):
+    # Iterate through the elements of the array from the first element to the second-to-last element
     for i in range(0, N - 1):
-        mini = i
+        mini = i  # Assume the current element is the minimum
+        
+        # Find the index of the minimum element in the unsorted portion
         for j in range(i + 1, N):
             if Arr[j] < Arr[mini]:
                 mini = j
+        
+        # Swap the minimum element with the first element in the unsorted portion
         temp = Arr[i]
         Arr[i] = Arr[mini]
         Arr[mini] = temp
+
+    # Return the sorted array
     return Arr
 
+# Take input from the user for the size of the array (N) and the array elements (Arr)
 N = int(input("Input size of array: "))
-Arr = list(map(int,input("Input Array: ").split()))
-res = selectionSort(Arr, N)
-print("Sorted Array:",*res)
+Arr = list(map(int, input("Input Array: ").split()))
 
+# Call the selectionSort function to sort the array
+res = selectionSort(Arr, N)
+
+# Print the sorted array
+print("Sorted Array:", *res)
 
 '''
 Input:-
