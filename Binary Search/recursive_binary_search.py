97e72f5952e678149302a9091747088086736d9d
Yash Chaturvedi <yashchaturvedi@WealthyINs-MacBook-Pro.local>
2025-04-29 18:34:12 +0530

    added new file for recursive binary search

---
+++ Binary Search/recursive_binary_search.py
@@ -0,0 +1,26 @@
+def RecursiveBinarySearch(nums, low, high, target):
+    # Base Condition
+    if low > high:
+        return - 1
+    mid = (low + high) // 2
+    if nums[mid] == target:
+        return mid
+    elif nums[mid] > target:
+        return RecursiveBinarySearch(nums, low, mid-1,target)
+    else:
+        return RecursiveBinarySearch(nums, low+1, high,target)
+
+def search(nums, target):
+    n = len(nums)
+    low = 0
+    high = n - 1
+    return RecursiveBinarySearch(nums, low, high, target)
+
+arr = list(map(int, input("Enter sorted array elements: ").split()))
+target = int(input("Enter element to be searched: "))
+
+result = search(arr, target)
+if result != -1:
+    print(f"Index of the target element: {result}")
+else:
+    print("Element not found!!!")
