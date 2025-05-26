64e253356b6149fa54fd7e611db572da4ac27d96
Yash Chaturvedi <yashchaturvedi@WealthyINs-MacBook-Pro.local>
2025-05-26 21:23:02 +0530

    updated code for search in rotated sorted array

--- Search in Rotated Sorted Array.py
+++ Search in Rotated Sorted Array.py
@@ -1,39 +1,40 @@
-# Python Code to search in a rotated sorted array
+from typing import List
 
-def binary_search(nums, target, left, right):
-    while left <= right:
-        mid = (left + right) // 2
-        if nums[mid] == target:
-            return mid
-        elif nums[mid] > target:
-            right = mid - 1
-        else:
-            left = mid + 1
-    return -1
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        low = 0
+        high = len(nums) - 1
+        while(low<=high):
+            mid = (low+high)//2
+            if target == nums[mid]:
+                return mid
+            # Left Sorted
+            if nums[low] <= nums[mid]:
+                if nums[low] <= target and target <= nums[mid]:
+                    high = mid - 1
+                else:
+                    low = mid + 1
+            # Right Sorted
+            else:
+                if nums[mid] <= target and target <= nums[high]:
+                    low = mid + 1
+                else:
+                    high = mid - 1
+        return -1
+                
+nums = [4,5,6,7,0,1,2]
+target = 0
+sol = Solution()
+ans = sol.search(nums, target)
 
-def find_min_index(nums):
-    n = len(nums)
-    left, right = 0, n - 1
-    if nums[left] < nums[right]:
-        return 0
-    while left <= right:
-        mid = left + (right - left) // 2
-        next_idx = (mid + 1) % n
-        prev_idx = (mid + n - 1) % n
-        if nums[mid] <= nums[next_idx] and nums[mid] <= nums[prev_idx]:
-            return mid
-        if nums[mid] <= nums[right]:
-            right = mid - 1
-        elif nums[left] <= nums[mid]:
-            left = mid + 1
-    return -1
+'''
+🕒 Time Complexity
 
-def search(nums, target):
-    n = len(nums)
-    min_index = find_min_index(nums)
-    result1 = binary_search(nums, target, 0, min_index - 1)
-    result2 = binary_search(nums, target, min_index, n - 1)
-    if result1 == -1:
-        return result2
-    else:
-        return result1
+-> Binary Search Iterations: In each iteration, the algorithm halves the search space by comparing the target with the middle element and determining which half is properly sorted. This results in a logarithmic number of iterations.
+-> Overall Time Complexity: O(log n), where n is the number of elements in the array.
+This efficiency stems from the binary search mechanism, which consistently reduces the search interval by half.
+
+🧠 Space Complexity
+-> Auxiliary Space: The algorithm uses a constant amount of extra space for variables like low, high, and mid.
+-> Overall Space Complexity: O(1) The search is conducted in place without utilizing additional data structures.
+'''
